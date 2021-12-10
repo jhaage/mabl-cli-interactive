5fc7cb38081ef58bf5320fb504bfe76b88cdbae4
Jeremy Haage <jeremy.haage@xandr.com>
2021-12-10 12:55:29 -0800

    playing with inquirer

--- src/index.js
+++ src/index.js
@@ -1,2 +1,47 @@
 #!/usr/bin/env node
-console.log("WIP");
+const inquirer = require('inquirer');
+const mabl = require('@mablhq/mabl-cli');
+const { exec } = require('child_process');
+//const MABL_AUTH = process.env.MABL_AUTH;
+//const MABL_WORKSPACE = process.env.MABL_WORKSPACE;
+
+function listTests() {
+  exec('mabl tests list', (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  })
+};
+
+inquirer
+  .prompt([
+    {
+      type: 'rawlist',
+      name: 'main',
+      message: 'Select an Option',
+      choices: [
+        'List Tests',
+        'Run a Test',
+        new inquirer.Separator(),
+        'Set Workspace',
+        'Set Mabl Login',
+        'Set Mabl AUTH Token'
+      ]
+    }
+  ])
+  .then((answers) => {
+    switch(answers.main) {
+      case 'List Tests':
+        console.log(`Running command:  mabl tests list`);
+        listTests();
+        break;
+      default:
+        console.log(`You answered with something else...`);
+    };
+  });
